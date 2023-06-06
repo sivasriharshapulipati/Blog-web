a9580a8ab0437371f344f7ad41270e3891e42993
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-06-06 14:01:31 +0530

    10 chalenges fin

--- v1/app.js
+++ v1/app.js
@@ -9,15 +9,34 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/",function(req,res){
+  res.render("home" ,{StartingContent:homeStartingContent})
+});
 
+app.get("/about",function(req,res){
+  res.render("about",{aboutcon:aboutContent});
+});
 
+app.get("/contact",function(req,res){
+  res.render("contact",{Contentcon:contactContent});
+});
 
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
+
+app.post("/compose",function(req,res){
+  const post = {
+    title:req.body.postTitle,
+    content: req.body.postBody
+  };
+
+});
 
 
 
