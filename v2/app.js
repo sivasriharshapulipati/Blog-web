77cc435a96d3d0197a7d6339d89f16af769be710
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-06-27 23:26:08 +0530

    there is a problem in opening posts , fix it

--- v2/app.js
+++ v2/app.js
@@ -3,8 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
-const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,22 +16,73 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://0.0.0.0:27017/todolistDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect("mongodb://0.0.0.0:27017/blogDB", {useNewUrlParser: true});
 
 const postSchema = {
-
   title: String,
- 
   content: String
- 
- };
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+  Post.find({})
+    .then(posts => {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("error"); // Render an error page in case of an error
+    });
+});
+
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error"); // Render an error page in case of an error
+    });
+});
+
+
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+
+  if (!mongoose.Types.ObjectId.isValid(requestedPostId)) {
+    res.render("error"); // Render an error page if the postId is invalid
+    return;
+  }
+
+  Post.findOne({_id: requestedPostId})
+    .then(post => {
+      if (!post) {
+        res.render("error"); // Render an error page if the post is not found
+      } else {
+        res.render("post", {
+          title: post.title,
+          content: post.content
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("error"); // Render an error page in case of an error
     });
 });
 
@@ -42,38 +92,6 @@ app.get("/about", function(req, res){
 
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
-});
-
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-
-  posts.push(post);
-
-  res.redirect("/");
-
-});
-
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
-  });
-
 });
 
 app.listen(3000, function() {
